f97b824033a8a1d7f0ccbd5b8655c28a7a54c4ec
venky7777 <44083185+sai-teja-7777@users.noreply.github.com>
2025-06-16 23:57:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -294,7 +294,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://www.youtube.com/embed/_eyFvp9R_j4?controls=0&loop=1&autoplay=1");
+  ifrm.setAttribute("src", "https://www.youtube.com/embed/KGTfYzDNNeg?controls=0&loop=1&autoplay=1");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
